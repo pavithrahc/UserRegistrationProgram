2a225ff3b9cac0d3a306c33d7b502d2f845e4be0
Pavithrahc <pavithramubbu93@gmail.com>
2022-10-01 16:47:09 +0530

    Added Test case For validating first name

--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -1,7 +1,22 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.regex.Pattern;
+
 public class UserRegistrationTest {
-    public static void main(String[] args) {
-        System.out.println("Welcome to User Registration Program");
-    }
+   @Test
+    public void givenFirstname_WhenProper_ShouldReturn_True(){
+   UserRegestration uservalid = new UserRegestration();
+   boolean result = uservalid.validatFirstName("Pavithra");
+       Assert.assertEquals(true,result);
+   }
+   @Test
+    public void givenFirstname_WhenNotProper_ShouldReturn_False(){
+       UserRegestration uservalid = new UserRegestration();
+       boolean result = uservalid.validatFirstName("pavi");
+       Assert.assertEquals(false,result);
+
+   }
 }
