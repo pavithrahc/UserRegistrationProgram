6ed9db211596caac611c8b5ccbe8ee58dab348cb
Pavithrahc <pavithramubbu93@gmail.com>
2022-10-01 17:14:52 +0530

    Added Test case For validating last name

--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -6,17 +6,32 @@ import org.junit.Test;
 import java.util.regex.Pattern;
 
 public class UserRegistrationTest {
-   @Test
-    public void givenFirstname_WhenProper_ShouldReturn_True(){
-   UserRegestration uservalid = new UserRegestration();
-   boolean result = uservalid.validatFirstName("Pavithra");
-       Assert.assertEquals(true,result);
-   }
-   @Test
-    public void givenFirstname_WhenNotProper_ShouldReturn_False(){
-       UserRegestration uservalid = new UserRegestration();
-       boolean result = uservalid.validatFirstName("pavi");
-       Assert.assertEquals(false,result);
+    @Test
+    public void givenFirstname_WhenProper_ShouldReturn_True() {
+        UserRegestration uservalid = new UserRegestration();
+        boolean result = uservalid.validatFirstName("Pavithra");
+        Assert.assertEquals(true, result);
+    }
 
-   }
+    @Test
+    public void givenFirstname_WhenNotProper_ShouldReturn_False() {
+        UserRegestration uservalid = new UserRegestration();
+        boolean result = uservalid.validatFirstName("pavi");
+        Assert.assertEquals(false, result);
+
+    }
+
+    @Test
+    public void givenLastName_WhenProper_ShouldReturn_True() {
+        UserRegestration uservalid = new UserRegestration();
+        boolean result = uservalid.validateLastName("Muddu");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenLastName_WhenNotProper_ShouldReturn_False() {
+        UserRegestration uservalid = new UserRegestration();
+        boolean result = uservalid.validateLastName("@");
+        Assert.assertEquals(false, result);
+    }
 }
