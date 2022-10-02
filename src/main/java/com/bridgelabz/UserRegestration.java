592cbccdf986c9b97fcbc7dbcd4ee5d98862df29
Pavithrahc <pavithramubbu93@gmail.com>
2022-10-02 09:08:01 +0530

    ADDED throw custom exceptions in case of invalid User Details

--- src/main/java/com/bridgelabz/UserRegestration.java
+++ src/main/java/com/bridgelabz/UserRegestration.java
@@ -1,38 +1,126 @@
 package com.bridgelabz;
-
+import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 public class UserRegestration {
-    private static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
-    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9.+_-]+[@][a-zA-Z0-9]+[.]co(m|.in)$";
-    private static final String EMAIL_PATTERN1 = "^[a-zA-Z0-9+]+[@][a-zA-Z0-9]+[.]co(m|.in)$";
-    private static final String PHONE_PATTERN = "^[0-9]{2}[0-9]{10}";
-    private static final String PASSWORD_PATTERN = "(?=.*[0-9])(?=.*[A-Z]).{8,40}";
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    public boolean validatFirstName(String firstName){
-        Pattern pattern = Pattern.compile(NAME_PATTERN);
-        return pattern.matcher(firstName).matches();
-    }
-    public boolean validateLastName(String lastName) {
-        Pattern pattern = Pattern.compile(NAME_PATTERN);
-        return pattern.matcher(lastName).matches();
-    }
-    public boolean validateEmail(String email) {
-        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-        return pattern.matcher(email).matches();
-    }
-    public boolean validateEmail1(String email) {
-        Pattern pattern = Pattern.compile(EMAIL_PATTERN1);
-        return pattern.matcher(email).matches();
-    }
-    public boolean validatePhone(String phone) {
-        Pattern pattern = Pattern.compile(PHONE_PATTERN);
-        return pattern.matcher(phone).matches();
-    }
-    public boolean validatePassword(String password) {
-        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
-        return pattern.matcher(password).matches();
+        System.out.print("Enter First Name : ");
+        String firstName = scanner.next();
+        System.out.println(validateName(firstName));
+
+        System.out.print("Enter Last Name : ");
+        String lastName = scanner.next();
+        System.out.println(validateName(lastName));
+
+        System.out.print("Enter Email : ");
+        String email = scanner.next();
+        System.out.println(validateEmail(email));
+
+        System.out.print("Enter Mobile Number : ");
+        String mNumber = scanner.next();
+        System.out.println(validatingMobile(mNumber));
+
+        System.out.print("Enter Password : ");
+        String password = scanner.next();
+        System.out.println(validatePassword(password));
     }
 
+    /**
+     * Method for validating the user first and last  name using regex.
+     *
+     * @param name : user input as first or last name
+     * @return : true or false
+     */
+    public static boolean validateName(String name) {
+        Pattern pattern = Pattern.compile("^[A-Z]{1}+[a-z]{2,}$");
+        if (name == null) {
+            return false;
+        }
+        Matcher matcher = pattern.matcher(name);
+        if (matcher.matches())
+            return true;
+        else
+            try {
+                throw new ValidationException("Invalid Name");
+            } catch (ValidationException e) {
+                e.printStackTrace();
+            }
+        return matcher.matches();
+    }
+
+
+    /**
+     * Method for validating the user's email using regex
+     *
+     * @param email : user's email
+     * @return : true or false
+     */
+    public static boolean validateEmail(String email) {
+        Pattern pattern = Pattern.compile("^[a-zA-z0-9.+-]+[@]+[a-zA-z0-9]+[.]+[a-zA-z0-9.]{2,}$");
+        if (email == null) {
+            return false;
+        }
+        Matcher matcher = pattern.matcher(email);
+        if (matcher.matches())
+            return true;
+        else
+            try {
+                throw new ValidationException("Invalid Email");
+            } catch (ValidationException e) {
+                e.printStackTrace();
+            }
+        return matcher.matches();
+    }
+
+    /**
+     * Method for validating the user's mobile number using regex
+     *
+     * @param mNumber : User's mobile number
+     * @return : true or false
+     */
+    public static boolean validatingMobile(String mNumber) {
+        Pattern pattern = Pattern.compile("^[9][1]+\\s\\d{10}$");
+        if (mNumber == null) {
+            return false;
+        }
+        Matcher matcher = pattern.matcher(mNumber);
+        if (matcher.matches())
+            return true;
+        else
+            try {
+                throw new ValidationException("Invalid Mobile Number");
+            } catch (ValidationException e) {
+                e.printStackTrace();
+            }
+        return matcher.matches();
+    }
+
+    /**
+     * Method for validating the user's password using regex.
+     * Rule1 – minimum 8 characters
+     * Rule2 – Should have at least 1 Upper Case - NOTE – All rules must be passed
+     * Rule3 – Should have at least 1 numeric number in the password
+     * Rule4 – Has exactly 1 Special Character
+     *
+     * @param password : user password
+     * @return : true or false.
+     */
+    public static boolean validatePassword(String password) {
+        Pattern pattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$");
+        if (password == null) {
+            return false;
+        }
+        Matcher matcher = pattern.matcher(password);
+        if (matcher.matches())
+            return true;
+        else
+            try {
+                throw new ValidationException("Invalid Password");
+            } catch (ValidationException e) {
+                e.printStackTrace();
+            }
+        return matcher.matches();
+    }
 }
-
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -1,87 +1,137 @@
 package com.bridgelabz;
-
-import org.junit.Assert;
+import com.bridgelabz.UserRegestration;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.regex.Pattern;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class UserRegistrationTest {
-    @Test
-    public void givenFirstname_WhenProper_ShouldReturn_True() {
-        UserRegestration uservalid = new UserRegestration();
-        boolean result = uservalid.validatFirstName("Pavithra");
-        Assert.assertEquals(true, result);
-    }
+    UserRegestration userRegistration;
 
-
-    @Test
-    public void givenFirstname_WhenNotProper_ShouldReturn_False() {
-        UserRegestration uservalid = new UserRegestration();
-        boolean result = uservalid.validatFirstName("pavi");
-        Assert.assertEquals(false, result);
-
-    }
-    @Test
-    public void givenLastName_WhenProper_ShouldReturn_True() {
-        UserRegestration userValidater = new UserRegestration();
-        boolean result = userValidater.validateLastName("Muddu");
-        Assert.assertEquals(true,result);
-    }
-    @Test
-    public void givenLastName_WhenNotProper_ShouldReturn_False() {
-        UserRegestration userValidater = new UserRegestration();
-        boolean result = userValidater.validateLastName("pavi");
-        Assert.assertEquals(false,result);
-    }
-    @Test
-    public void givenEmail_WhenProper_ShouldReturn_True() {
-        UserRegestration userValidater = new UserRegestration();
-        boolean result = userValidater.validateEmail("abc.xyz@bl.co.in");
-        Assert.assertEquals(true,result);
+    @Before
+    public void setup() {
+        userRegistration = new UserRegestration();
     }
 
     @Test
-    public void givenEmail_WhenNotProper_ShouldReturn_False() {
-       UserRegestration userValidater = new UserRegestration();
-        boolean result = userValidater.validateEmail("pavi@.com");
-        Assert.assertEquals(false,result);
-    }
-    @Test
-    public void givenEmail1_WhenProper_ShouldReturn_True() {
-        UserRegestration userValidater = new UserRegestration();
-        boolean result = userValidater.validateEmail1("abcxyz@bl.co.in");
-        Assert.assertEquals(true,result);
+    public void validateName_shouldReturnTrue_whenFirstLatterIsCapital() {
+        boolean result = userRegistration.validateName("Pavithra");
+        assertTrue(result);
     }
 
     @Test
-    public void givenEmail1_WhenNotProper_ShouldReturn_False() {
-        UserRegestration userValidater = new UserRegestration();
-        boolean result = userValidater.validateEmail1("pavi@.com");
-        Assert.assertEquals(false,result);
+    public void validateName_shouldReturnFalse_whenNameFirstLatterIsSmall() {
+        boolean result = userRegistration.validateName("pavithra");
+        assertFalse(result);
     }
+
     @Test
-    public void givenPhone_WhenProper_ShouldReturn_True() {
-        UserRegestration userValidater = new UserRegestration();
-        boolean result = userValidater.validatePhone("917259924438");
-        Assert.assertEquals(true,result);
+    public void validateName_shouldReturnTrue_whenNameStartsWithCapAndHasMini3Characters() {
+        boolean result = userRegistration.validateName("Pavi");
+        assertTrue(result);
     }
+
     @Test
-    public void givenPhone_WhenNotProper_ShouldReturn_False() {
-        UserRegestration userValidater = new UserRegestration();
-        boolean result = userValidater.validatePhone("7259924438");
-        Assert.assertEquals(false,result);
+    public void validateName_shouldReturnFalse_whenNameStartsWithCapAndHasLessThan3Characters() {
+        boolean result = userRegistration.validateName("Hc");
+        assertFalse(result);
     }
+
     @Test
-    public void givenPassword_WhenProper_ShouldReturn_True() {
-        UserRegestration userValidater = new UserRegestration();
-        boolean result = userValidater.validatePassword("abcdfdA@#fg0");
-        Assert.assertEquals(true,result);
+    public void validateName_shouldReturnFalse_whenNameIsNull() {
+        boolean result = userRegistration.validateName(null);
+        assertFalse(result);
     }
+
     @Test
-    public void givenPassword_WhenNotProper_ShouldReturn_False() {
-        UserRegestration userValidater = new UserRegestration();
-        boolean result = userValidater.validatePassword("123asdfgh");
-        Assert.assertEquals(false,result);
+    public void validateEmail_shouldReturnFalse_whenEmailIsNull() {
+        boolean result = userRegistration.validateEmail(null);
+        assertFalse(result);
+    }
+
+    @Test
+    public void validateEmail_shouldReturnFalse_whenEmailMissAnyOfMandatoryParts() {
+        boolean result = userRegistration.validateEmail("@bl.co.in");
+        assertFalse(result);
+    }
+
+    @Test
+    public void validateEmail_shouldReturnFalse_whenEmailMissATSIGN() {
+        boolean result = userRegistration.validateEmail("abc.xyz.co.in");
+        assertFalse(result);
+    }
+
+    @Test
+    public void validateEmail_shouldReturnTrue_whenEmailHasAllMandatoryParts() {
+        boolean result = userRegistration.validateEmail("abc.xyz@bl.co.in");
+        assertTrue(result);
+    }
+
+    @Test
+    public void validateMobile_shouldReturnFalse_whenMobileNumbersAreNull() {
+        boolean result = userRegistration.validatingMobile(null);
+        assertFalse(result);
+    }
+
+    @Test
+    public void validateMobile_shouldReturnFalse_whenCountryCodeIsMissing() {
+        boolean result = userRegistration.validatingMobile("7259924438");
+        assertFalse(result);
+    }
+
+    @Test
+    public void validateMobile_shouldReturnTrue_whenCountryCodeIsMentioned() {
+        boolean result = userRegistration.validatingMobile("91 7259924438");
+        assertTrue(result);
+    }
+
+    @Test
+    public void validateMobile_shouldReturnFalse_whenCountryCodeIsMentionedWithoutSpace() {
+        boolean result = userRegistration.validatingMobile("917259924438");
+        assertFalse(result);
+    }
+
+    @Test
+    public void validateMobile_shouldReturnFalse_whenNumbersIsLessInCount() {
+        boolean result = userRegistration.validatingMobile("91 89883");
+        assertFalse(result);
+    }
+
+    @Test
+    public void validatePassword_shouldReturnFalse_whenPasswordIsNull() {
+        boolean result = userRegistration.validatePassword(null);
+        assertFalse(result);
+    }
+
+    @Test
+    public void validatePassword_shouldReturnTrue_whenPasswordProper() {
+        boolean result = userRegistration.validatePassword("Ah5f@djs");
+        assertTrue(result);
+    }
+
+    @Test
+    public void validatePassword_shouldReturnFalse_whenPasswordMissUpperCaseCharacter() {
+        boolean result = userRegistration.validatePassword("as2@cfr");
+        assertFalse(result);
+    }
+
+    @Test
+    public void validatePassword_shouldReturnFalse_whenPasswordMissNumericCharacter() {
+        boolean result = userRegistration.validatePassword("Ah@jscfr");
+        assertFalse(result);
+    }
+
+    @Test
+    public void validatePassword_shouldReturnFalse_whenPasswordIsLessThan8Characters() {
+        boolean result = userRegistration.validatePassword("Ah@js");
+        assertFalse(result);
+    }
+
+    @Test
+    public void validatePassword_shouldReturnFalse_whenPasswordMissSpecialCharacter() {
+        boolean result = userRegistration.validatePassword("Ah5fkdjs");
+        assertFalse(result);
     }
 
 }
